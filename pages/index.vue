a8809c37c088d73f807c76e05ab1083c24962a62
ValentinSaraiva-MMI <valentin.saraiva@edu.univ-fcomte.fr>
2023-11-21 01:06:02 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>hello</h1>
+<h2 class="test">test</h2>
+
+</template>
+
+<style lang="scss">
+
+.test {
+    text-align: center;
+  color: $primary-color;
+}
+
+</style>
+
+<script></script>
